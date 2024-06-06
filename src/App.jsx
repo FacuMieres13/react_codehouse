d9535edd02bf43f18223724025d295de192ac011
FacuMieres13 <aguscora00@gmail.com>
2024-06-06 12:37:52 -0300

    creando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css'
+import { NavBar } from './components/NavBar'
+
+function App() {
+  const condition = true
+  let result = null
+// IF NORMAL PERO DEMASIADAS LINEAS
+// if(condition){
+//   result = "correct"
+// }else {
+//   result = "incorrect"
+// }
+// console.log(`This is ${result}`)
+
+
+
+// IF TERNARIO SUGAR SYNTAXS
+  // console.log(`This is ${condition ? "correct" : "incorrect"}`)
+
+  // SPREED OPETAROR
+
+  // const arra1 = ["A", "B", "C"]
+  // const arra2 = ["D", "E", "F"]
+
+  // console.log(arra1 + "")
+  // MOSTRAR EN STRING
+  // console.log(...arra1)
+  // const arrayNuevo = 
+  let curso = 12345
+  return (
+    <>
+    <h1>Hola mundo</h1>
+    <h2>Estamos en el custo de coder</h2>
+    <h2>Curso: {curso}</h2>
+    <NavBar />
+    </>
+  )
+}
+
+export default App
